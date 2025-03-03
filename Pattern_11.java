eb9ff031e0fd0bbf6c2fa24f85d4d159964743ea
Bani Kumari <bannikumari03@gmail.com>
2025-03-03 12:29:55 +0530

    Add files via upload

---
+++ Pattern_11.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Pattern_11
+{
+public static void main(String args[])
+{
+int i,j,p;
+for(i=1;i<=5;i++)
+{
+for(j=1;j<=5-i;j++)
+{
+System.out.print(" ");
+}
+p=1;
+for(j=1;j<=i;j++)
+{
+System.out.print(p++);
+}
+p=p-2;
+for(j=1;j<=i-1;j++)
+{
+System.out.print(p--);
+
+}
+System.out.println();
+}
+}
+}
